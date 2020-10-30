96f5c3b1e5fa1b1ff694650c6d1175bbf60e199a
exyrias <25100303+exyrias@users.noreply.github.com>
2020-10-30 13:21:09 +0900

    add example code

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,24 @@
 //!         end_elem!("p"; "Text goes here..."),
 //! );
 //! assert_eq!("<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>Page Title</title></head><body><h1>Section</h1><p>Text goes here...</p></body></html>", html.render());
+//! 
+//! let th = elem!("tr";
+//!             end_elem!("th"; "Item1"),
+//!             end_elem!("th"; "Item2"),
+//!             end_elem!("th"; "Item3"),
+//! );
+//! let tr1 = elem!("tr";
+//!             end_elem!("td"; "value 11"),
+//!             end_elem!("td"; "value 12"),
+//!             end_elem!("td"; "value 13"),
+//! );
+//! let tr2 = elem!("tr";
+//!             end_elem!("td"; "value 21"),
+//!             end_elem!("td"; "value 22"),
+//!             end_elem!("td"; "value 23"),
+//! );
+//! let tbl = elem!("table"; ("border", "1"); th, tr1, tr2);
+//! assert_eq!("<table border=\"1\"><tr><th>Item1</th><th>Item2</th><th>Item3</th></tr><tr><td>value 11</td><td>value 12</td><td>value 13</td></tr><tr><td>value 21</td><td>value 22</td><td>value 23</td></tr></table>", tbl.render());
 //! ```
 //! 
 //! DOM Nodes are represented by `enum DomNode`, which can be created by macros, e.g.,
