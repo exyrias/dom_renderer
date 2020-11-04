0fcad003dde35417ad60220a8054e3cb9e42a9bd
exyrias <25100303+exyrias@users.noreply.github.com>
2020-11-04 11:10:25 +0900

    implement as_XXX() for DomNode

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-//! Utilities for generating html (or other DOM) strings 
-//! 
+//! Utilities for generating html (or other DOM) strings
+//!
 //! # Example
-//! 
+//!
 //! A simple html page can be created by following codes:
-//! 
+//!
 //! ```
 //! use dom_renderer::*;
 //! let html = html_basic!(
@@ -13,7 +13,7 @@
 //!         end_elem!("p"; "Text goes here..."),
 //! );
 //! assert_eq!("<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>Page Title</title></head><body><h1>Section</h1><p>Text goes here...</p></body></html>", html.render());
-//! 
+//!
 //! let th = elem!("tr";
 //!             end_elem!("th"; "Item1"),
 //!             end_elem!("th"; "Item2"),
@@ -32,18 +32,18 @@
 //! let tbl = elem!("table"; ("border", "1"); th, tr1, tr2);
 //! assert_eq!("<table border=\"1\"><tr><th>Item1</th><th>Item2</th><th>Item3</th></tr><tr><td>value 11</td><td>value 12</td><td>value 13</td></tr><tr><td>value 21</td><td>value 22</td><td>value 23</td></tr></table>", tbl.render());
 //! ```
-//! 
+//!
 //! DOM Nodes are represented by `enum DomNode`, which can be created by macros, e.g.,
-//! 
+//!
 
 pub mod doc_type;
-pub mod element;
 pub mod document;
+pub mod element;
 pub mod macros;
 
 pub use doc_type::DomDocType;
-pub use element::{DomEmptyElem, DomElem};
 pub use document::DomDocument;
+pub use element::{DomElem, DomEmptyElem};
 pub use macros::*;
 
 /// Render recursive elements to String instance
@@ -69,12 +69,54 @@ impl Render for DomNode {
             DocumentType(n) => n.render(),
             EmptyElement(n) => n.render(),
             Element(n) => n.render(),
-            Text(n) => n.to_string(),
+            Text(n) => n.clone(),
         }
     }
 }
 
 impl DomNode {
+    pub fn as_doc(&self) -> Option<&DomDocument> {
+        match *self {
+            Document(ref e) => Some(e),
+            _ => None,
+        }
+    }
+
+    pub fn as_doc_mut(&mut self) -> Option<&mut DomDocument> {
+        match *self {
+            Document(ref mut e) => Some(e),
+            _ => None,
+        }
+    }
+
+    pub fn as_doctype(&self) -> Option<&DomDocType> {
+        match *self {
+            DocumentType(ref e) => Some(e),
+            _ => None,
+        }
+    }
+
+    pub fn as_doctype_mut(&mut self) -> Option<&mut DomDocType> {
+        match *self {
+            DocumentType(ref mut e) => Some(e),
+            _ => None,
+        }
+    }
+
+    pub fn as_empty(&self) -> Option<&DomEmptyElem> {
+        match *self {
+            EmptyElement(ref e) => Some(e),
+            _ => None,
+        }
+    }
+
+    pub fn as_empty_mut(&mut self) -> Option<&mut DomEmptyElem> {
+        match *self {
+            EmptyElement(ref mut e) => Some(e),
+            _ => None,
+        }
+    }
+
     pub fn as_elem(&self) -> Option<&DomElem> {
         match *self {
             Element(ref e) => Some(e),
@@ -85,6 +127,20 @@ impl DomNode {
     pub fn as_elem_mut(&mut self) -> Option<&mut DomElem> {
         match *self {
             Element(ref mut e) => Some(e),
+            _ => None,
+        }
+    }
+
+    pub fn as_text(&self) -> Option<&String> {
+        match *self {
+            Text(ref e) => Some(e),
+            _ => None,
+        }
+    }
+
+    pub fn as_text_mut(&mut self) -> Option<&mut String> {
+        match *self {
+            Text(ref mut e) => Some(e),
             _ => None,
         }
     }
--- tests/ref.rs
+++ tests/ref.rs
@@ -4,6 +4,45 @@ mod ref_test {
     use dom_renderer::*;
 
     #[test]
+    fn document() {
+        let a = domdoc!(end_elem!("a"));
+        let b = empty!("b");
+
+        assert_eq!("<a></a>", a.as_doc().unwrap().render());
+        assert!(b.as_doc().is_none());
+
+        let mut a = a;
+        a.as_doc_mut().unwrap().nodes.push(domtxt!("text"));
+        assert_eq!("<a></a>text", a.as_doc().unwrap().render());
+    }
+
+    #[test]
+    fn document_type() {
+        let a = doctype!("html");
+        let b = empty!("b");
+
+        assert_eq!("<!DOCTYPE html>", a.as_doctype().unwrap().render());
+        assert!(b.as_doctype().is_none());
+
+        let mut a = a;
+        a.as_doctype_mut().unwrap().doc_type = "root";
+        assert_eq!("<!DOCTYPE root>", a.as_doctype().unwrap().render());
+    }
+
+    #[test]
+    fn empty() {
+        let a = empty!("a");
+        let b = end_elem!("b");
+
+        assert_eq!("<a>", a.as_empty().unwrap().render());
+        assert!(b.as_empty().is_none());
+
+        let mut a = a;
+        a.as_empty_mut().unwrap().attributes.push(("id",String::from("id1")));
+        assert_eq!("<a id=\"id1\">", a.as_empty().unwrap().render());
+    }
+
+    #[test]
     fn element() {
         let a = end_elem!("a");
         let b = empty!("b");
@@ -11,9 +50,22 @@ mod ref_test {
         assert_eq!("<a></a>", a.as_elem().unwrap().render());
         assert!(b.as_elem().is_none());
 
-        let mut c = end_elem!("c");
-        c.as_elem_mut().unwrap().child_nodes.push(domtxt!("text"));
-        assert_eq!("<c>text</c>", c.as_elem().unwrap().render());
+        let mut a = a;
+        a.as_elem_mut().unwrap().child_nodes.push(domtxt!("text"));
+        assert_eq!("<a>text</a>", a.as_elem().unwrap().render());
+    }
+
+    #[test]
+    fn text() {
+        let a = domtxt!("text");
+        let b = empty!("b");
+
+        assert_eq!("text", a.as_text().unwrap());
+        assert!(b.as_text().is_none());
+
+        let mut a = a;
+        a.as_text_mut().unwrap().replace_range(..4, "TEXT");
+        assert_eq!("TEXT", a.as_text().unwrap());
     }
 
     #[test]
@@ -29,6 +81,9 @@ mod ref_test {
         let elem_c = elem_n.child_nodes[1].as_elem_mut().unwrap();
         assert_eq!("c", elem_c.tag);
         elem_c.child_nodes.push(empty!("child"));
-        assert_eq!("<a><b>BValue</b><c>CValue<child></c><d>DValue</d></a>", n.render());
+        elem_c.attributes.push(("attr", String::from("ATTR")));
+        elem_c.child_nodes.push(end_elem!("child2"; "Child"));
+        elem_c.child_nodes[2].as_elem_mut().unwrap().child_nodes[0].as_text_mut().unwrap().push_str("Value");
+        assert_eq!("<a><b>BValue</b><c attr=\"ATTR\">CValue<child><child2>ChildValue</child2></c><d>DValue</d></a>", n.render());
     }
 }
